3c10999ab61bb32a2b4cd7fb3502930e32a54857
Luispe <38361000+lpbonomi@users.noreply.github.com>
2024-08-26 20:13:37 +0200

    obtener monedas

---
+++ src/monedas.ts
@@ -0,0 +1,23 @@
+import { getSoapClient } from './soapClient';
+
+const groups = {
+    'INTERNATIONAL': 1,
+    'LOCAL': 2,
+    'LOCAL_RATES': 5,
+    'ALL': 0
+}
+
+type Group = keyof typeof groups;
+
+interface WsMonedasOutLinea {
+    Codigo: number;
+    Nombre: string;
+}
+
+export async function obtenerMonedas(group: Group = 'ALL') {
+    const client = await getSoapClient('awsbcumonedas');
+
+    const [result] = await client.ExecuteAsync({ Entrada: { Grupo: groups[group] } });
+
+    return result.Salida['wsmonedasout.Linea'] as WsMonedasOutLinea[]
+}
--- src/soapClient.ts
+++ src/soapClient.ts
@@ -1,6 +1,6 @@
 import { createClientAsync } from 'soap';
 
-type WebService = 'awsultimocierre' | 'awsbcucotizaciones'
+type WebService = 'awsultimocierre' | 'awsbcumonedas' | 'awsbcucotizaciones'
 
 export async function getSoapClient(ws: WebService) {
     return await createClientAsync(`https://cotizaciones.bcu.gub.uy/wscotizaciones/servlet/${ws}?wsdl`);
